67bc787f53644c8b16d78d41f25dd6d5cffe6d2e
frenchfritzy <fritz32973@gmail.com>
2026-03-29 20:09:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -57,4 +57,6 @@ def view_students():
     return render_template('view_students.html', students=students)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))  # Render will set PORT
+    app.run(host='0.0.0.0', port=port, debug=True)
